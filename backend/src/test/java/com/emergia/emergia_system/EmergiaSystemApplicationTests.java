cc339ec12c6ed7fb0ad0e8acf9ee3c4e3ea7d608
Pietra Valentina <100318524+PietraValen@users.noreply.github.com>
2025-03-30 20:54:25 +0000

    novas config

---
+++ backend/src/test/java/com/emergia/emergia_system/EmergiaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergia.emergia_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergiaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
